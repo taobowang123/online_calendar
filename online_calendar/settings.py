566d79190b4d1b29e74ad51a875b814d8a0d0173
botao <841226510@qq.com>
2021-01-01 00:20:32 +0800

    v8

--- online_calendar/settings.py
+++ online_calendar/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '&q675j^@k$$3-vul5r2hk8zp=s@ej8_z=%@wrjcpc&t=r1-+u&'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.73.134.20']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '54.73.134.20']
 
 
 # Application definition
